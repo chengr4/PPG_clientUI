ff97f2834d6071e327a710d0348003416ca81e2f
Eddie Cheng <you@example.com>
2020-02-25 16:27:37 +0800

    add measure function (not finished yet)

--- app/src/main/java/com/example/measureapp/MainActivity.java
+++ app/src/main/java/com/example/measureapp/MainActivity.java
@@ -4,11 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    // from HTTP library Volley
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * sends the raw data to server and gets sbp, dbp, hr back
+     */
+    public void getMeasure() {
+        // url to post and get
+        String url = "https://drf-firsttry.herokuapp.com/paradigms/";
+
+        // key-value to post
+        HashMap data = new HashMap();
+        data.put("name","7D");
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(data), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        mQueue.add(postRequest);
     }
 }
